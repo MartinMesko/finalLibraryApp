cf47b919c3bad4f72184ab80aba67bb83ffdc237
MartinMesko <alegremato@gmail.com>
2023-08-14 13:15:22 +0200

    Refactoring.

--- src/main/java/sk/itvkurze/Lekcia_15/LibraryApp.java
+++ src/main/java/sk/itvkurze/Lekcia_15/LibraryApp.java
@@ -10,7 +10,6 @@ public class LibraryApp {
 
         TitlesPage titlesPage = new TitlesPage(scanner);
 
-
         do {
             System.out.println(">>>> Welcome to our Library <<<<");
             System.out.println("1 - Titles");
@@ -20,40 +19,41 @@ public class LibraryApp {
             System.out.println("5 - Exit");
             System.out.print("Choose an option: ");
 
-            if (scanner.hasNextInt()) { // kontrola, či je nasledujúce vstupné číslo
-                choice = scanner.nextInt();
-                scanner.nextLine();// Pridaný riadok pre očistenie vstupu
-                switch (choice) {
-                    case 1:
-                        titlesPage.display();
-                        break;
-                    case 2:
-                        MembersPage.showPage();
-                        break;
-                    case 3:
-                        // prejde Rentals submenu
-                        break;
-                    case 4:
-                        // prejde Messages submenu
-                        break;
-                    case 5:
-                        System.out.println("Exiting application...");
-                        break;
-                    default:
-                        System.out.println("Please enter a number in the range from 1 to 5");
-                        break;
-                }
-            } else { // používateľ zadal neplatný vstup
+            String input = scanner.nextLine();
+            try {
+                choice = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
                 System.out.println("Please enter a valid value.");
-                scanner.next(); // prečítanie neplatného vstupu z konzoly, aby sa zabránilo nekonečnému cyklovaniu
-                choice = 0; // nastavenie choice na 0, aby sa cyklus opakoval
+                choice = 0; // aby sa cyklus opakoval
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    titlesPage.display();
+                    break;
+                case 2:
+                    MembersPage.showPage();
+                    break;
+                case 3:
+                    // prejde Rentals submenu
+                    break;
+                case 4:
+                    // prejde Messages submenu
+                    break;
+                case 5:
+                    System.out.println("Exiting application...");
+                    break;
+                default:
+                    System.out.println("Please enter a number in the range from 1 to 5");
+                    break;
             }
         } while (choice != 5);
 
-       // scanner.close();
+        // scanner.close();  // v prípade, že sa scanner zavrie tu, môže spôsobiť problémy v iných častiach programu, kde sa tento scanner používa
     }
 
     public static void showMainMenu() {
+        // Táto metóda je prázdna, môžete ju buď doplniť alebo odstrániť, ak nie je potrebná.
     }
-
 }
--- src/test/java/sk/itvkurze/Lekcia_15/LibraryAppTest.java
+++ src/test/java/sk/itvkurze/Lekcia_15/LibraryAppTest.java
@@ -11,5 +11,48 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LibraryAppTest {
+    private TitlesPage titlesPage;
 
+    private final String lineSeparator = System.lineSeparator();
+
+    @BeforeEach
+    public void setUp() {
+        titlesPage = new TitlesPage(null); // null je tu v poriadku, pre tento test nepotrebujeme objekt typu Scanner
+
+        // Nastavenie testovacích súborov so vzorovými údajmi
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("titles.txt"))) {
+            writer.write("Book1,Author1,ISBN1,300,5" + lineSeparator);
+            writer.write("Book2,Author2,ISBN2,500,3" + lineSeparator);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("titlesDVD.txt"))) {
+            writer.write("DVD1,AuthorDVD1,10,90,2" + lineSeparator);
+            writer.write("DVD2,AuthorDVD2,15,120,0" + lineSeparator);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Test
+//    public void testShowAllTitlesWithTitlesAvailable() {
+//        List<String> result = titlesPage.showAlltitles();
+//
+//        assertEquals(5, result.size());
+//        assertEquals("All Titles", result.get(0));
+//        assertEquals("Name: Book1 - Author: Author1 | ISBN: ISBN1 | Number of pages: 300 | Available copies: 5", result.get(1));
+//        assertEquals("Name: Book2 - Author: Author2 | ISBN: ISBN2 | Number of pages: 500 | Available copies: 3", result.get(2));
+//        assertEquals("Name: DVD1 - Author: AuthorDVD1 - Number of chapters: 10 - Length in minutes: 90 | Available copies: 2", result.get(3));
+//        assertEquals("Name: DVD2 - Author: AuthorDVD2 - Number of chapters: 15 - Length in minutes: 120 | Available copies: 0", result.get(4));
+//    }
+//
+//    @Test
+//    public void testShowAllTitlesWhenNoTitlesAvailable() {
+//        // Predpokladáme, že ak súbory "titles.txt" a "titlesDVD.txt" neexistujú, vráti "No titles available"
+//        List<String> result = titlesPage.showAlltitles();
+//
+//        assertEquals(1, result.size());
+//        assertEquals("No titles available", result.get(0));
+//    }
 }
